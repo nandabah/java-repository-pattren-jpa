5da8d079b79124b18b003ab80d92cfa64a58f088
Dafa Sr <dafashanrizki2200@gmail.com>
2025-04-19 23:36:23 +0700

    feat: add Teacher entity with relationships to Course

---
+++ src/main/java/id/nan/entity/Teacher.java
@@ -0,0 +1,29 @@
+package id.nan.entity;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "m_teachers")
+@Builder
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Course> courses;
+
+}
