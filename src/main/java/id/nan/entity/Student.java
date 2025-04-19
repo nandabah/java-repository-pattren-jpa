92cbd655d4bdf6c6e12d0fc8563c784f90fb63d4
Dafa Sr <dafashanrizki2200@gmail.com>
2025-04-19 23:36:09 +0700

    feat: add Student entity with relationships to Course

---
+++ src/main/java/id/nan/entity/Student.java
@@ -0,0 +1,27 @@
+package id.nan.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "m_students")
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String email;
+
+    @ManyToMany(mappedBy = "students")
+    private List<Course> courses;
+
+}
