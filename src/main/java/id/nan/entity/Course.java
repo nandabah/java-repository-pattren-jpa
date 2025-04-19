24552569e3b47b177a590ad00a38621e5b038d8d
Dafa Sr <dafashanrizki2200@gmail.com>
2025-04-19 23:35:59 +0700

    feat: add Course entity with relationships to Student and Teacher

---
+++ src/main/java/id/nan/entity/Course.java
@@ -0,0 +1,28 @@
+package id.nan.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "m_courses")
+@Builder
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "t_courses_students", joinColumns = @JoinColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private List<Student> students;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+}
